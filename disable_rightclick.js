f7bd73dc933761be20417c18b7b1ba3e2a3f779f
Jayce <102136659@qq.com>
2018-10-10 16:24:02 +0800

    Create disable_rightclick.js

---
+++ disable_rightclick.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         禁止右键点击
+// @namespace    102136659@qq.com
+// @version      1.0
+// @description  适用于noVNC界面
+// @author       jayce
+// @include      http://localhost:6901/*
+// @include      http://127.0.0.1:6901/*
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+    'use strict';
+    window.oncontextmenu = function () {
+        console.log("Right Click Disabled");
+        return false;
+    }
+})();
+
